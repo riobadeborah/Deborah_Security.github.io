4be523f03d5332a5fa1319d151d9c875f46384ab
Deborah Rioba <debrawetu@gmail.com>
2023-12-10 16:19:30 -0400

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,15 @@
+// auth.js
+function authenticateUser() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Replace with your actual authentication credentials
+    if (username === 'IoT NSCC' && password === 'Leeds@ 70367') {
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('fileUploader').style.display = 'block';
+        document.getElementById('accessLogSection').style.display = 'block';
+        connectToMQTT(); // Proceed to connect to MQTT after successful authentication
+    } else {
+        alert('Invalid username or password. Please try again.');
+    }
+}
